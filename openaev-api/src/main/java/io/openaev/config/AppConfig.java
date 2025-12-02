da1391c7312d2161c170ed1224213b3720023244
EvaE-Filigran <eva.ezzeribi@filigran.io>
2025-12-02 09:13:12 +0100

    [backend] chore: add login swagger with JSESSIONID (#4279)

--- openaev-api/src/main/java/io/openaev/config/AppConfig.java
+++ openaev-api/src/main/java/io/openaev/config/AppConfig.java
@@ -2,10 +2,13 @@ package io.openaev.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.openaev.helper.ObjectMapperHelper;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -37,6 +40,7 @@ public class AppConfig {
 
   @Bean
   public OpenAPI openAEVOpenAPI() {
+    final String securitySchemaName = "JSESSIONID";
     return new OpenAPI()
         .info(
             new Info()
@@ -45,6 +49,15 @@ public class AppConfig {
                     "Software under open source licence designed to plan and conduct exercises")
                 .version(this.openAEVConfig.getVersion())
                 .license(new License().name("Apache 2.0").url("https://filigran.io//")))
+        .addSecurityItem(new SecurityRequirement().addList(securitySchemaName))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    securitySchemaName,
+                    new SecurityScheme()
+                        .name(securitySchemaName)
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.COOKIE)))
         .externalDocs(
             new ExternalDocumentation()
                 .description("OpenAEV documentation")
